0bca0c0b0c2618529a7c78139c2e1ead4d8eaf7a
Nieopodal <mail.macieja.poltorak@gmail.com>
2023-02-19 16:46:37 +0100

    Added config.example.ts and established a database connection.

--- index.ts
+++ index.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import {handleError} from "./utils/error";
+import {config} from "./config/config";
 
 const app = express();
 
 app
     .use(cors({
-        origin: 'http://localhost:3000',
+        origin: config.corsOrigin,
     }))
     .use(express.json());
 
