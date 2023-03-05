fecd40a5e2fd91454810d9c71e7e5fb8abbd3fd5
Nieopodal <mail.macieja.poltorak@gmail.com>
2023-03-05 16:26:58 +0100

    Added more test conditions to see if the createNewOperationsFromRepetitiveHandler function is appropriate.

--- tests/operation-handlers.ts/create-new-operation-from-repetitive-handler.test.ts
+++ tests/operation-handlers.ts/create-new-operation-from-repetitive-handler.test.ts
@@ -9,24 +9,35 @@ afterAll(async () => {
 
 test('createNewOperationFromRepetitiveHandler can get all user`s root repetitive operations and create new ones which belongs to given period.', async () => {
     const newPeriod = new PeriodRecord({
-        budgetAmount: 5000, freeCashAmount: 0, isActive: false, paymentsAmount: 0, savingsAmount: 5000, userId: "[test-user-id-for-handlers]"
+        budgetAmount: 5000,
+        freeCashAmount: 0,
+        isActive: false,
+        paymentsAmount: 0,
+        savingsAmount: 5000,
+        userId: "[test-user-id-for-handlers]"
     });
     await newPeriod.insert();
     const rootOp = new OperationRecord({
-        amount: -50, isRepetitive: true, type: 1, category: 1, subcategory: 1, userId: newPeriod.userId, description: 'test-description-for-handler-testing',
+        amount: -50,
+        isRepetitive: true,
+        type: 1,
+        category: 1,
+        subcategory: 1,
+        userId: newPeriod.userId,
+        description: 'test-description-for-handler-testing',
+        createdAt: '2023-01-01 12:00:00',
     });
     await rootOp.insert();
 
-    setTimeout(async () => {
-        await createNewOperationsFromRepetitiveHandler(newPeriod.id, newPeriod.userId);
-        const found = await OperationRecord.findPeriodOperations(newPeriod.id, newPeriod.userId);
-        expect(found.length).toBe(1);
-        expect(found[0].periodId).toBe(newPeriod.id);
-        expect(found[0].originId).toBe(rootOp.id);
-        expect(found[0].createdAt).not.toBe(rootOp.createdAt);
+    await createNewOperationsFromRepetitiveHandler(newPeriod.id, newPeriod.userId);
+    const found = await OperationRecord.findPeriodOperations(newPeriod.id, newPeriod.userId);
+    expect(found.length).toBe(1);
+    expect(found[0].periodId).toBe(newPeriod.id);
+    expect(found[0].originId).toBe(rootOp.id);
+    expect(found[0].createdAt).not.toBe(rootOp.createdAt);
 
-        await newPeriod.delete();
-        await rootOp.delete();
-        await found[0].delete();
-    }, 1000);
+    await newPeriod.delete();
+    await rootOp.delete();
+    await found[0].delete();
+
 });
